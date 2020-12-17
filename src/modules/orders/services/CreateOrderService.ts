71dc2e8ff12a1940653e86e6992a27ca270e01cb
luanrem <luanrem@gmail.com>
2020-12-17 19:05:32 -0300

    Minha alteracao

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -6,6 +6,7 @@ import IProductsRepository from '@modules/products/repositories/IProductsReposit
 import ICustomersRepository from '@modules/customers/repositories/ICustomersRepository';
 import Order from '../infra/typeorm/entities/Order';
 import IOrdersRepository from '../repositories/IOrdersRepository';
+import ICreateOrderDTO from '../dtos/ICreateOrderDTO';
 
 interface IProduct {
   id: string;
@@ -20,13 +21,103 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const findProducts = products.map(product => {
+      return {
+        id: product.id,
+      };
+    });
+
+    const productsList = await this.productsRepository.findAllById(
+      findProducts,
+    );
+
+    console.log('produto', productsList);
+
+    const productsToCreate = products.map(product => {
+      return {
+        product_id: product.id,
+        price: productsList.filter(p => (p.id === product.id ? p.price : null)),
+        quantity: product.quantity,
+      };
+    });
+
+    const customer = await this.customersRepository.findById(customer_id);
+
+    const resultProduct: ICreateOrderDTO = {
+      customer,
+      products: productsToCreate,
+    };
+
+    if (customer && products) {
+      await this.ordersRepository.create({
+        customer,
+        products: productsToCreate,
+      });
+    }
+
+    // customer: Customer;
+    // products: IProduct[
+    //   {
+    //     product_id: string;
+    //     price: number;
+    //     quantity: number;
+    //   },
+    //   {
+    //     product_id: string;
+    //     price: number;
+    //     quantity: number;
+    //   },
+    // ];
+
+    // const newProduct = products.map(product => {
+    //   return {
+    //     product.id,
+    //     price
+    //   }
+    // })
+
+    // const newProduct = products.map(product => {
+    //   const price = await this.productsRepository.findAllById([product.id])
+
+    //   return {
+    //     product.id,
+    //     price,
+    //     product.quantity,
+    //   }
+    // })
+
+    //   product_id: string;
+    // price: number;
+    // quantity: number;
+
+    // const order = await this.ordersRepository.create({
+    //   customer: customer_id,
+    //   products,
+    // });
+
+    // const customerExist = await this.customersRepository.findByEmail(email);
+
+    // if (customerExist) {
+    //   throw new AppError('The customer already exists.');
+    // }
+
+    // const customer = await this.customersRepository.create({
+    //   name,
+    //   email,
+    // });
+
+    // return customer;
   }
 }
 
---
+++ src/shared/infra/typeorm/migrations/1607993664399-AddProductsIdToOrdersProdutcs.ts
@@ -0,0 +1,39 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class AddProductsIdToOrdersProdutcs1607993664399
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'order_products',
+      new TableColumn({
+        name: 'product_id',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'order_products',
+      new TableForeignKey({
+        name: 'OrdersProductsProduct',
+        columnNames: ['product_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'products',
+        onDelete: 'SET NULL',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey(
+      'orders_products',
+      'OrdersProductsProduct',
+    );
+    await queryRunner.dropColumn('orders_products', 'product_id');
+  }
+}
